8c29c1f4724d3d1b57f0bac0d5a7fc121ca93385
Marc Harris <mharris@choicestream.com>
2016-02-13 03:05:11 -0500

    dump db

--- definitions/views.py
+++ definitions/views.py
@@ -4,4 +4,4 @@ import locale
 
 def index(request):
     locales = "Current locale: %s %s -- Default locale: %s %s" % (locale.getlocale() + locale.getdefaultlocale())
-    return HttpResponse("locales: " + locales)
+    return HttpResponse("<p>This is card game.</p><p>There's nothing here yet.</p><p>locales: " + locales)
