863ad22b3b28593066af0a2abf2a48c4e0bc8716
Marc Harris <mharris@choicestream.com>
2016-02-19 14:57:22 -0400

    Double  underscore for ordering by a field in a related table. That's obvious. First case so far of "I know the SQL query, now how do I get django to to it". After a mere 4 lines of django querying. Well done django. Not.

--- instances/views.py
+++ instances/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from .models import GameSession
 
 def index(request):
-    sessions = GameSession.objects.order_by('game_definition')[:10]
+    sessions = GameSession.objects.order_by('game_definition__name')[:10]
     output = ', '.join([str(session) for session in sessions])
 
 #    locales = "Current locale: %s %s -- Default locale: %s %s" % (locale.getlocale() + locale.getdefaultlocale())
